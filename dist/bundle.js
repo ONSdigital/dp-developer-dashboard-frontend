31004781fd8edd311257b9195a7f4fbd5c636617
Jon Dewi Jones <jondewijones@gmail.com>
2017-07-27 15:07:39 +0100

    Update app to render componeents based on state (temporary)

--- dist/bundle.js
+++ dist/bundle.js
@@ -83,7 +83,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-__WEBPACK_IMPORTED_MODULE_1_react_dom__["render"](__WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_2__App__["a" /* default */], { text: "Body text" }), document.getElementById('root'));
+__WEBPACK_IMPORTED_MODULE_1_react_dom__["render"](__WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_2__App__["a" /* default */], { text: "New text" }), document.getElementById('root'));
 
 /***/ }),
 /* 2 */
@@ -93,6 +93,70 @@ module.exports = ReactDOM;
 
 /***/ }),
 /* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_alert_alert__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_notification_notification__ = __webpack_require__(5);
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+
+
+
+
+
+var App = function (_Component) {
+    _inherits(App, _Component);
+
+    function App(props) {
+        _classCallCheck(this, App);
+
+        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+        _this.state = {
+            alerts: [{
+                title: "Test alert",
+                description: "this is a test alert",
+                date: Date.now()
+            }],
+            notifications: [{
+                title: "Test notification",
+                description: "this is a test notification",
+                date: Date.now(),
+                type: "Deployment"
+            }]
+        };
+        return _this;
+    }
+
+    _createClass(App, [{
+        key: "render",
+        value: function render() {
+            var alerts = this.state.alerts || [];
+            var notifications = this.state.notifications || [];
+            return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("div", null, alerts.map(function (alert, index) {
+                return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_1__components_alert_alert__["a" /* default */], Object.assign({ id: index }, alert));
+            }), notifications.map(function (notification, index) {
+                return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"](__WEBPACK_IMPORTED_MODULE_2__components_notification_notification__["a" /* default */], Object.assign({ id: index }, notification));
+            }));
+        }
+    }]);
+
+    return App;
+}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
+
+/* harmony default export */ __webpack_exports__["a"] = (App);
+
+/***/ }),
+/* 4 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -109,43 +173,71 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 
 
-var App = function (_Component) {
-    _inherits(App, _Component);
+var Alert = function (_Component) {
+    _inherits(Alert, _Component);
 
-    function App(props) {
-        _classCallCheck(this, App);
+    function Alert(props) {
+        _classCallCheck(this, Alert);
 
-        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
-
-        _this.state = {
-            time: ""
-        };
-        return _this;
+        return _possibleConstructorReturn(this, (Alert.__proto__ || Object.getPrototypeOf(Alert)).call(this, props));
+        // this.state = {
+        //     time: ""
+        // }
     }
 
-    _createClass(App, [{
-        key: "componentWillMount",
-        value: function componentWillMount() {
-            var _this2 = this;
-
-            setInterval(function () {
-                var date = new Date();
-                _this2.setState({
-                    time: date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
-                });
-            }, 100);
-        }
-    }, {
+    _createClass(Alert, [{
         key: "render",
         value: function render() {
-            return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("div", null, __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("h1", null, "Hello world"), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", null, this.props.text), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", null, this.state.time));
+            return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("div", { className: "alert" }, __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("h1", { className: "alert__title" }, this.props.title), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", { className: "alert__description" }, this.props.description), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", { className: "alert__date" }, this.props.date));
         }
     }]);
 
-    return App;
+    return Alert;
 }(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
 
-/* harmony default export */ __webpack_exports__["a"] = (App);
+/* harmony default export */ __webpack_exports__["a"] = (Alert);
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+
+
+
+var Notification = function (_Component) {
+    _inherits(Notification, _Component);
+
+    function Notification(props) {
+        _classCallCheck(this, Notification);
+
+        return _possibleConstructorReturn(this, (Notification.__proto__ || Object.getPrototypeOf(Notification)).call(this, props));
+        // this.state = {
+        //     time: ""
+        // }
+    }
+
+    _createClass(Notification, [{
+        key: "render",
+        value: function render() {
+            return __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("div", { className: "notification" }, __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("h1", { className: "notification__title" }, this.props.title), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", { className: "notification__description" }, this.props.description), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", { className: "notification__date" }, this.props.date), __WEBPACK_IMPORTED_MODULE_0_react__["createElement"]("p", { className: "notification__type" }, this.props.type));
+        }
+    }]);
+
+    return Notification;
+}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
+
+/* harmony default export */ __webpack_exports__["a"] = (Notification);
 
 /***/ })
 /******/ ]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { Component } from "react";
+import { Alert, Notification } from "./models";
+import AlertComponent from "./components/alert/alert";
+import NotificationComponent from "./components/notification/notification";
 
 interface Props {
     text?: string
 }
 
 interface State {
-    time: string
+    alerts: Alert[],
+    notifications: Notification[]
 }
 
 export default class App extends Component<Props, State> {
@@ -14,29 +18,36 @@ export default class App extends Component<Props, State> {
         super(props);
 
         this.state = {
-            time: ""
+            alerts: [
+                {
+                    title: "Test alert",
+                    description: "this is a test alert",
+                    date: Date.now()
+                }
+            ],
+            notifications: [
+                {
+                    title: "Test notification",
+                    description: "this is a test notification",
+                    date: Date.now(),
+                    type: "Deployment"
+                }
+            ]
         }
     }
 
-    static defaultProps: Partial<Props> = {
-        text: "Default text"
-    }
-
-    componentWillMount(): void {
-        setInterval(() => {
-            const date: Date = new Date();
-            this.setState({
-                time: (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds())
-            });
-        }, 100);
-    }
-
-    render() {
+    render(): JSX.Element {
+        const alerts: Alert[] = this.state.alerts || [];
+        const notifications: Notification[] = this.state.notifications || [];
         return (
             <div>
-                <h1>Hello world</h1>
-                <p>{this.props.text}</p>
-                <p>{this.state.time}</p>
+                {alerts.map((alert: Alert, index: number): JSX.Element => {
+                    return (<AlertComponent id={index} {...alert}/>)
+                }) }
+
+                {notifications.map((notification: Notification, index: number): JSX.Element => {
+                    return (<NotificationComponent id={index} {...notification}/>)
+                }) }
             </div>
         );
     }
