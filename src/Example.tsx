5eae2ba3bb268bee02564e4132ae6453f03bd938
Jon Dewi Jones <jondewijones@gmail.com>
2017-07-27 14:20:19 +0100

    Add additional rules to type definition checking

---
+++ src/Example.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Component } from "react";
+
+interface Props {
+    text?: string
+}
+
+interface State {
+    time: string
+}
+
+export default class App extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            time: ""
+        }
+    }
+
+    static defaultProps: Partial<Props> = {
+        text: "Default text"
+    }
+
+    componentWillMount(): void {
+        setInterval(() => {
+            const date: Date = new Date();
+            this.setState({
+                time: (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds())
+            });
+        }, 100);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <p>{this.props.text}</p>
+                <p>{this.state.time}</p>
+            </div>
+        );
+    }
+}
